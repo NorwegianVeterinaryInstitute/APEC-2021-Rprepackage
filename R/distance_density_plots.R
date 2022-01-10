e7db665b8c7fd042ec6b1fd7ff9efc824e6c4b9c
evezeyl <evezeyl@gmail.com>
2022-01-10 16:53:49 +0100

    added title to each plot

--- R/distance_density_plots.R
+++ R/distance_density_plots.R
@@ -4,9 +4,13 @@
 #' example 
 #' @param distance_long_object distance 
 #' @param n_breaks to ajust the x axis breaks of density plots 
+#' @param title1 tile for density plot
+#' @param title2 title for cumulative density plot 
 
 #' depends patchwork
-distance_density_plots <- function(distance_long_object, n_breaks){
+distance_density_plots <- function(distance_long_object, n_breaks, 
+                                   title1 = "Density - Pairwise distance", 
+                                   title2 = "Cumulative density - Pairwise distance"){
     
     print("removing duplicated pairs and common ids to compute statistics")
     
@@ -27,13 +31,14 @@ distance_density_plots <- function(distance_long_object, n_breaks){
     
     density_plot <- ggplot(distance.long, aes(distance))  +
     geom_density() +
-    labs(x = "Pairwise distance") +
+    labs(x = "Pairwise distance", title = title1) +
     scale_x_continuous(breaks = scales::breaks_extended(n = n_breaks))
     
     cumulative_density_plot <- ggplot(distance.long, aes(distance))  +
     ggplot2::stat_ecdf() +
     labs(x = "Pairwise distance",
-         y = "cumulative density") +
+         y = "cumulative density",
+         title = title2) +
     scale_x_continuous(breaks = scales::breaks_extended(n = n_breaks))
     
     patchwork_plots <- density_plot / cumulative_density_plot
